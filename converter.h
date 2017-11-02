348ae093856d47c65291db93abe00e279910bd1e
graciestiney <31670203+graciestiney@users.noreply.github.com>
2017-11-02 15:04:18 -0500

    Update converter.h

--- converter.h
+++ converter.h
@@ -6,3 +6,11 @@ float cm_to_in(void)
     printf("\nThe number of inches is %.2f\n", in);
     return in;
 }
+float mi_to_m(void)
+{
+    //GRACE STINEMATES
+    float mi_to_m = get_float("Number of Miles: ");
+    float m = mi / 1909.34;
+    printf("\nThe number of meters is%2f\n", m);
+    return m;
+}
