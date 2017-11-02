e0f8e1870ef0eb538adcc6b3ff449d06f60d848f
nussbada <31670218+nussbada@users.noreply.github.com>
2017-11-02 15:04:17 -0500

    Update converter.h

--- converter.h
+++ converter.h
@@ -6,3 +6,11 @@ float cm_to_in(void)
     printf("\nThe number of inches is %.2f\n", in);
     return in;
 }
+float in_to_m(void)
+{
+    //ADAM NUSSBAUM
+    float in = get_float("Number of inches:");
+    float m = in * .0254;
+    printf("\nThe number of meters is %.2f\n", m);
+    return m;
+}
